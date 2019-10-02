d8b2e3555a39c3572aec146656b93a33cfe447b9
Ben Farquhar <bennicholsfarquhar@gmail.com>
2019-10-02 16:25:00 -0400

    big junk push

--- src/components/ChartBox.js
+++ src/components/ChartBox.js
@@ -1,15 +1,16 @@
 import React from "react";
-import BaseChart from "./Charts/BaseChart";
-import PieChart from "./Charts/PieChart";
-import LineChart from "./Charts/LineChart";
+// import BaseChart from "./Charts/BaseChart";
+// import PieChart from "./Charts/PieChart";
+// import LineChart from "./Charts/LineChart";
+import { ResponsivePie } from '@nivo/pie'
 import RadarChart from "./Charts/RadarChart"
 
 const ChartBox = props => {
   const PieChartData = () => {
-    let pieData = {};
+    let pieData = [];
     const data = props.data;
     data.members.map((mem, i) => {
-      pieData[mem] = data.getTotalMessages(data.getMessagesBySender(mem));
+      pieData.push({"id": mem, "label": mem, "value": data.getTotalMessages(data.getMessagesBySender(mem)) })
     });
     return pieData;
   };
@@ -48,13 +49,37 @@ const ChartBox = props => {
 
   return (
     <div className="charts">
-      <PieChart
+      <ResponsivePie
+        data = {PieChartData()}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        colors={{ scheme: 'nivo' }}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextXOffset={6}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={16}
+        radialLabelsLinkHorizontalLength={24}
+        radialLabelsLinkStrokeWidth={1}
+        radialLabelsLinkColor={{ from: 'color' }}
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor="#333333"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+      ></ResponsivePie>
+
+      {/* <PieChart
         data={PieChartData()}
         title="Messages by Sender"
         height={600}
         width={600}
         margin={50}
-      />
+      /> */}
       <RadarChart
         data={RadarTimeData()}
         width={600}
--- src/data/Processors/BaseDataProcessor.js
+++
@@ -1,8 +0,0 @@
-export default class BaseDataProcessor {
-  constructor(props) {
-  }
-  auth() {}
-  formatCSVData(input) {} //NOTE: format functions return chat objects
-  formatJsonData(input) {}
-  formatHTMLData(input){}
-}
